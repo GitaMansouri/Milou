18203d252ee42508c0b51a3ee16fc286b6758bc9
GitaMansouri <gita.mansouri.84@gmail.com>
2025-08-15 02:28:40 +0330

    build user model and add annotations

---
+++ src/main/java/model/User.java
@@ -0,0 +1,51 @@
+package model;
+
+
+import jakarta.persistence.Basic;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Basic(optional = false)
+    private String name;
+    @Basic(optional = false)
+    @Column(unique = true)
+    private String email;
+    @Basic(optional = false)
+    private String password;
+
+    public User() {
+    }
+    public User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
